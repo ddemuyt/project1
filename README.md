705557f847dcf2f4c65e78ea5c7460e687f8fa5f
Rayhana <rfazily@gmail.com>
2020-03-23 01:25:30 -0400

    hide show added and other styles

--- README.md
+++ README.md
@@ -1,2 +1,23 @@
-# project1
-Project 1 (due on Monday, 3/23/2020) using HTML, CSS (Bootstrap), JavaScript (jQuery), and Web APIs.
+# Project 01
+
+*description*
+
+## User Story
+
+```
+AS A music fan/concert goer/ foodie who enjoys planning
+I WANT to know the location and weather for my favorite bands/fests/concerts plus food options in the DMV area 
+SO THAT I can have the best musical experience EVER
+```
+
+## Acceptance Criteria
+
+```
+GIVEN an event dashboard with form inputs
+WHEN I search for a musical genre from the drop down menu
+THEN I receive a list of musical artists along with their concert dates and venues AND the current weather in Washington, DC
+WHEN I search in the google map on the bottom of the page
+THEN I am presented with food options in the venue's surrounding area
+
+- - -
+(c) 2020 Group 2 (Kevin Cortina, Rayhana Fazily, Drake DeMuyt, Javay Porter) - Project 1
--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+
 var rockKey = "KnvZfZ7vAeA";
 var EDMkey = "KnvZfZ7vAvF";
 var countryKey = "KnvZfZ7vAv6";
@@ -5,13 +6,20 @@ var folkKey = "KnvZfZ7vAva";
 var rapKey = "KnvZfZ7vAv1";
 var popKey = "KnvZfZ7vAev";
 var rnbKeyy = "KnvZfZ7vAee";
-
-var queryURL = "https://app.ticketmaster.com/discovery/v2/events.json?apikey=2AFGOLxDadbErVsemSWtnWf3BL6ENzT1&marketId=47&size=5&classificationName=music&genreId="
+var apiCall = "https://api.openweathermap.org/data/2.5/weather?appid=ecf05eb98aee0572391da1c4901b75ec&zip=20001,us";
+var queryURL = "https://app.ticketmaster.com/discovery/v2/events.json?apikey=2AFGOLxDadbErVsemSWtnWf3BL6ENzT1&postalCode=20001&size=5&classificationName=music&genreId="
 var fullQueryURL = ""
 
+getWeather();
+
 $("#submit").on("click", function(){
+
+
    $("#rockDiv").empty();
    console.log($("#select").attr("id"));
+
+   $('#results').addClass('show');
+
 
 switch ($("#select").val()){
    case "Rock":
@@ -56,11 +64,13 @@ $.ajax({
             for (var i = 0; i < json._embedded.events.length; i++){
                var eventsloop = json._embedded.events[i];
                var events = $('<div class="is-size-4 has-text-primary">').text(eventsloop.name);
+               var date = $('<h3>').text(eventsloop.dates.start.localDate);
                var venue = $("<h3>").text(eventsloop._embedded.venues[0].name);
                var address = $("<h4>").html(eventsloop._embedded.venues[0].address.line1 + "<br>" + eventsloop._embedded.venues[0].city.name + ", " + eventsloop._embedded.venues[0].state.name);
                // var tickets = $("<a>").attr("href", eventsloop.url);
                var d = $("<div>");
                d.append(events);
+               d.append(date);
                d.append(venue);
                d.append(address);
                d.append('<br>');
@@ -77,8 +87,54 @@ $.ajax({
  console.log($("#select").attr("id"));
 });
 
+function getWeather() {
+$.ajax({
+   url: apiCall,
+   method: "GET",
+})
+
+   .then(function (response) {
+       
+       //Log response object
+       console.log(response);
+
+       var tempF = (response.main.temp - 273.15) * 1.80 + 32;
+       var feelsliketempF = (response.main.feels_like - 273.15) * 1.80 + 32;
+
+       // add temp content to html
+       $("#temp").text("Temperature: " + tempF.toFixed(2) + " F");
+       $("#feelsliketemp").text("Feels Like: " + feelsliketempF.toFixed(2) + " F");
+       $("#wind").text("Wind Speed: " + response.wind.speed + " MPH");
+       $("#humidity").text("Humidity: " + response.main.humidity + "%");
 
 
+var iconURL = "http://openweathermap.org/img/wn/";
+
+
+var currentWC = response.weather[0].main;
+if (currentWC == "Clear"){
+ $("#currentimg").attr("src", iconURL + "01d@2x.png");
+}
+else if (currentWC == "Clouds"){
+ $("#currentimg").attr("src", iconURL + "03d@2x.png");
+}
+else if (currentWC == "Thunderstorm"){
+ $("#currentimg").attr("src", iconURL + "11d@2x.png");
+}
+else if (currentWC == "Drizzle"){
+ $("#currentimg").attr("src", iconURL + "09d@2x.png");
+}
+else if (currentWC == "Rain"){
+ $("#currentimg").attr("src", iconURL + "10d@2x.png");
+}
+else if (currentWC == "Snow"){
+ $("#currentimg").attr("src", iconURL + "13d@2x.png");
+}
+else {
+ $("#currentimg").attr("src", iconURL + "50d@2x.png");
+}
+   });
+};
 function initAutocomplete() {
    var map = new google.maps.Map(document.getElementById('map'), {
      center: {lat: 38.9072, lng: -77.0369},
@@ -138,4 +194,3 @@ function initAutocomplete() {
      map.fitBounds(bounds);
    });
  }
-
